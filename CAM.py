b37a85355be558ef123099eb5d6c775856603ff7
jiaqiFromPanther <daqizaiop@gmail.com>
2022-01-26 22:31:20 -0500

    visualize

--- CAM.py
+++ CAM.py
@@ -45,21 +45,20 @@ input_tensor = dataset[0]["image"]
 rgb_img = (np.float32(input_tensor.permute(1, 2, 0)))
 print(dataset[0]["labels"])
 plt.imshow(rgb_img); plt.axis('off'); plt.tight_layout(); plt.show()
-plt.savefig('origin.png')
 
 input_tensor = input_tensor.unsqueeze(0).to(device)
 output = model(input_tensor)
 print(output)
-
+# ['srf', 'irf', 'ezAtt', 'ezDis', 'hrd', 'rpe', 'rt', 'dril']
 # activation_map = cam_extractor(output.squeeze(0).argmax().item(), output)
-target = [ClassifierOutputTarget(1)]
-grayscale_cam = cam(input_tensor=input_tensor,targets=target,eigen_smooth=False, aug_smooth=False)
+target = [ClassifierOutputTarget(6)]
+grayscale_cam = cam(input_tensor=input_tensor,targets=target,eigen_smooth=False, aug_smooth=True)
 grayscale_cam = grayscale_cam[0, :]
 # Visualize the raw CAM
 plt.imshow(grayscale_cam); plt.axis('off'); plt.tight_layout(); plt.show()
-plt.savefig('foo.png')
+# plt.savefig('cam_test/irf.png')
 # print(grayscale_cam, grayscale_cam.shape, rgb_img.shape)
 # sss
 visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 plt.imshow(visualization); plt.axis('off'); plt.tight_layout(); plt.show()
-plt.savefig('foo2.png')
+plt.savefig('cam_test/ts.png')
